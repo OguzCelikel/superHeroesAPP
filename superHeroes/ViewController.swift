a82b7d936841c46e12aa83b0616673f8cf98f197
Ömer <omeroguzcelikel@gmail.com>
2021-11-30 13:42:28 +0300

    tableViewStarted

--- superHeroes/ViewController.swift
+++ superHeroes/ViewController.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+// UITableViewDelegate -> Table view management
+// UITableViewDataSource -> Our Data Source will give error. To solve it write functions (numburOfRowsInSection, cellForRow)
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView1: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    // numburOfRowsInSection -> how many rows
+    // cellForRow atIndexPath -> whats inside of these rows
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 }
 
